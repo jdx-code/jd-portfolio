d89c73141ae492f9a25bf0533e084fd0a7ef6973
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-01 22:47:10 +0530

    Added custom fonts to navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,8 @@ module.exports = {
 	theme: {		
 		extend: {
 			fontFamily: {
-    			playfair: "'Playfair Display', serif",
+    			playfair: "'Playfair Display', serif",				
+				gantari: "'Gantari', sans-serif",
 			},
 		},
 	},
